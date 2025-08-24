857a246123439f293d62f107815184a791fca055
Julia Butler <juliafulibutler@gmail.com>
2025-08-24 01:43:12 -0400

    Finished downloading data, preprocessing, and beginning eda

--- src/data/download.py
+++ src/data/download.py
@@ -4,11 +4,11 @@ import fastf1
 import fastf1.logger
 import time
 
-def download_schedule(year, include_testing=False):
+def download_schedule(year:int, include_testing:bool=False):
     schedule = fastf1.get_event_schedule(year, include_testing=include_testing)
     return schedule
 
-def download_session(year, gp, session_type='R'):
+def download_session(year:int, gp:str, session_type:str='R'):
     session = fastf1.get_session(year, gp, session_type)
     session.load()
     return session
@@ -16,6 +16,10 @@ def download_session(year, gp, session_type='R'):
 def get_locations_from_schedule(schedule):
     locations = schedule['Location'].tolist()
     return locations
+
+def get_locations_with_sprint_from_schedule(schedule):
+    sprint_locations = schedule[schedule['EventFormat'].str.contains('sprint', case=False, na=False)]
+    return sprint_locations['Location'].tolist()
 
 def get_rounds_from_schedule(schedule):
     rounds = schedule['RoundNumber'].tolist()
@@ -37,22 +41,41 @@ if __name__ == "__main__":
 
         # Get the schedule for the year
         schedule = download_schedule(year)
-        print(schedule[['RoundNumber', 'EventName', 'Location']])
+        print(schedule[['RoundNumber', 'EventName', 'Location', 'EventFormat']])
+        print()
+
         # Get the round numbers from the schedule
         schedule_rounds = get_rounds_from_schedule(schedule)
         schedule_locations = get_locations_from_schedule(schedule)
+        sprint_locations = get_locations_with_sprint_from_schedule(schedule)
 
         for round in schedule_rounds:
-            print(f" > Downloading data for Round {round} - {schedule_locations[round - 1]}")
+            print(f" > Downloading data for Round {round} - {schedule_locations[round - 1]} | Is a Sprint Event? {True if schedule_locations[round - 1] in sprint_locations else False}")
 
             # For each round, download the results from the session
             gp_session = download_session(year, round, 'R') # for this project, we only want the race results
             gp_session.load()
 
+            if gp_session is None or gp_session.results is None or gp_session.results.empty:
+                print(f"   - [yellow]WARNING[/yellow]: No race results data returned for [red]Round {round} - {schedule_locations[round - 1]}[/red], skipping...", end='\n\n')
+                continue
+
             # Save the results to a CSV file in the raw data directory
             data_file_name = f'{year}_Round_{round}_{schedule_locations[round - 1]}_results.csv'
             gp_session.results.to_csv(f'../../data/raw/{data_file_name}', index=False)
             print(f"   - Saved results to [green]data/raw/{data_file_name}[/green]", end='\n\n')
+
+            if schedule_locations[round - 1] in sprint_locations:
+                s_session = download_session(year, round, 'S')
+                s_session.load()
+
+                if s_session is None or s_session.results is None or s_session.results.empty:
+                    print(f"   - [yellow]WARNING[/yellow]: Although this was a sprint event, no sprint results data returned for [red]Round {round} - {schedule_locations[round - 1]}[/red], skipping...", end='\n\n')
+                    continue
+
+                sprint_file_name = f'{year}_Round_{round}_{schedule_locations[round - 1]}_sprint_results.csv'
+                s_session.results.to_csv(f'../../data/raw/{sprint_file_name}', index=False)
+                print(f"   - Saved sprint results to [green]data/raw/{sprint_file_name}[/green]", end='\n\n')
     
     download_end_time = time.time()
     total_download_time = download_end_time - download_start_time
---
+++ src/data/preprocess.py
@@ -0,0 +1,66 @@
+from rich import print
+
+import os
+import pandas as pd
+
+RAW_DATA_PATH = "../../data/raw/"
+PREPROCESSED_DATA_PATH = "../../data/preprocessed/"
+RAW_DATA_FILES = os.listdir(RAW_DATA_PATH)
+
+def get_data_files_for_year(year:str):
+    return [file for file in RAW_DATA_FILES if file.startswith(str(year))]
+
+def combine_data_files_for_year(file_list:list):
+    year_data_df = pd.DataFrame()
+    for file in file_list:
+        file_path = os.path.join(RAW_DATA_PATH, file)
+        file_df = pd.read_csv(file_path)
+
+        # Add a column for the year extracted from the file name
+        file_year = file.split('_')[0]
+        file_df.insert(loc=0, column='Year', value=file_year)
+
+        # Add a column for the round number extracted from the file name
+        gp_round = file.split('_')[2]
+        gp_round = int(gp_round) if gp_round.isdigit() else gp_round
+        file_df.insert(loc=1, column='Round', value=gp_round)
+
+        # Add a column for the grand prix location extracted from the file name
+        gp_location = file.split('_')[3]
+        file_df.insert(loc=2, column='Location', value=gp_location)
+
+        year_data_df = pd.concat([year_data_df, file_df], ignore_index=True)
+    return year_data_df.sort_values(by='Round')
+
+def autofill_driver_data_given_id(all_data_df:pd.DataFrame, driver_ids:list, id_column:str):
+    result_df = pd.DataFrame()
+    for driver in driver_ids:
+        all_results = all_data_df[all_data_df[id_column] == driver]
+        all_results = all_results[['DriverId', 'FirstName', 'LastName', 'FullName', 'Abbreviation', 'BroadcastName']].drop_duplicates().dropna()
+        result_df = pd.concat([result_df, all_results], ignore_index=True)
+    return result_df.sort_values(by='DriverId').reset_index(drop=True)
+
+if __name__ == "__main__":
+    print()
+    years = [2018, 2019, 2020, 2021, 2022, 2023, 2024]
+    all_data_df = pd.DataFrame()
+
+    print("Starting data preprocessing...")
+    for year in years:
+        print(f" > Processing data for the {year} season")
+        files = get_data_files_for_year(year)
+        year_df = combine_data_files_for_year(files)
+
+        all_data_df = pd.concat([all_data_df, year_df], ignore_index=True)
+
+        preprocessed_file_name = f'{year}_season_results.csv'
+        year_df.to_csv(os.path.join(PREPROCESSED_DATA_PATH, preprocessed_file_name), index=False)
+        print(f"   - Saved preprocessed data to [green]data/preprocessed/{preprocessed_file_name}[/green]", end='\n\n')
+
+    print("Gathering data on drivers...")
+    unique_drivers = all_data_df['DriverId'].unique()
+    drivers_df = autofill_driver_data_given_id(all_data_df, unique_drivers, 'DriverId')
+    drivers_df.to_csv(os.path.join(PREPROCESSED_DATA_PATH, 'drivers.csv'), index=False)
+    print(f"   - Saved drivers data to [green]data/preprocessed/drivers.csv[/green]", end='\n\n')
+    
+    print("Data preprocessing completed.")
