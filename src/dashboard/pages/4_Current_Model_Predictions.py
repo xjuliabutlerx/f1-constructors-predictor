d0556722941a8bf91d8d1aa62477772fb0ae84b6
Julia Butler <juliafulibutler@gmail.com>
2025-10-22 07:32:23 -0400

    Updating predictions post-US Grand Prix

--- src/dashboard/pages/4_Current_Model_Predictions.py
+++ src/dashboard/pages/4_Current_Model_Predictions.py
@@ -6,19 +6,19 @@ st.set_page_config(page_title="Current Predictions", page_icon="📈", layout="w
 
 st.title("📈 Current 2025 Constructors' Championship Ranking Predictions")
 
-st.write("As of October 5, 2025, McLaren has won the 2025 World Constructors' Championship. However, the remaining 9 ranks are still yet to be determined. The following predictions were run with data collected for all the races in the 2025 season up through the Singapore Grand Prix.")
+st.write("As of October 5, 2025, McLaren has won the 2025 World Constructors' Championship. However, the remaining 9 ranks are still yet to be determined. The following predictions were run with data collected for all the races in the 2025 season up through the United States Grand Prix in Austin, Texas.")
 
-st.write("While the results from all 3 model ensembles are presented, the overall, most accurate predictions come from the v3 generation. The projections from v2 and v1 should be used to provide the viewer with a more holistic picture of recent trends and long-term outlook to help guide any inferences.")
+st.write("While the results from all 3 model ensembles are presented, the overall, most statistically accurate predictions come from the v3 generation. The projections from v2 and v1 should be used to provide the viewer with a more holistic picture of recent trends and long-term outlook to help guide any inferences.")
 
 cwd = os.getcwd()
 
-V1_PREDICTIONS = pd.read_csv(os.path.join("predictions", "2025-10-07 v1 ensemble predictions.csv"))
-V2_PREDICTIONS = pd.read_csv(os.path.join("predictions", "2025-10-07 v2 ensemble predictions.csv"))
-V3_PREDICTIONS = pd.read_csv(os.path.join("predictions", "2025-10-07 v3 ensemble predictions.csv"))
+V1_PREDICTIONS = pd.read_csv(os.path.join("predictions", "2025-10-22 v1 ensemble predictions.csv"))
+V2_PREDICTIONS = pd.read_csv(os.path.join("predictions", "2025-10-22 v2 ensemble predictions.csv"))
+V3_PREDICTIONS = pd.read_csv(os.path.join("predictions", "2025-10-22 v3 ensemble predictions.csv"))
 
-# V1_PREDICTIONS = pd.read_csv(os.path.join(cwd, "..", "..", "predictions", "2025-10-07 v1 ensemble predictions.csv"))
-# V2_PREDICTIONS = pd.read_csv(os.path.join(cwd, "..", "..", "predictions", "2025-10-07 v2 ensemble predictions.csv"))
-# V3_PREDICTIONS = pd.read_csv(os.path.join(cwd, "..", "..", "predictions", "2025-10-07 v3 ensemble predictions.csv"))
+# V1_PREDICTIONS = pd.read_csv(os.path.join(cwd, "..", "..", "predictions", "2025-10-22 v1 ensemble predictions.csv"))
+# V2_PREDICTIONS = pd.read_csv(os.path.join(cwd, "..", "..", "predictions", "2025-10-22 v2 ensemble predictions.csv"))
+# V3_PREDICTIONS = pd.read_csv(os.path.join(cwd, "..", "..", "predictions", "2025-10-22 v3 ensemble predictions.csv"))
 
 V1_PREDICTIONS = V1_PREDICTIONS[["Team", "Current Points", "Monaco Model", "Silverstone Model", "Suzuka Model", "Spa-Francorchamps Model", "Baku Model"]]
 V2_PREDICTIONS = V2_PREDICTIONS[["Team", "Current Points", "Monaco Model v2", "Silverstone Model v2", "Suzuka Model v2", "Spa-Francorchamps Model v2", "Baku Model v2"]]
@@ -29,8 +29,9 @@ st.subheader("*The Best of Both Worlds - balancing recency and season-long consi
 st.dataframe(V3_PREDICTIONS, width="content", hide_index=True)
 st.markdown('''
 Key Points:
-- Since there are only 6 races remaining, the models are predicting less movement from the current positions.
-- Despite Ferrari's declining performance, the models do not think Red Bull Racing will be able take 3rd place from Ferrari. This is likely due to the fact that the Ferrari drivers both earn points at each race unlike Red Bull Racing.
+- Despite the poor race results from Mercedes, the models are not suggesting that any team could take 2nd place from them.
+- However, there will be a true battle for 3rd between Red Bull Racing and Ferrari, given the former's recent surge in race wins, podiums, and overall points earnings.
+- The Monaco Model, the most accurate of this ensemble, predicts Red Bull Racing will come out on top of Ferrari but just behind Mercedes.
 ''')
 
 st.header("v2 Model Ensemble Predictions")
@@ -38,10 +39,9 @@ st.subheader("*Slow and Steady Wins the Race - focused on long-term performances
 st.dataframe(V2_PREDICTIONS, width="content", hide_index=True)
 st.markdown('''
 Key Points:
-- After McLaren, Mercedes is strongly predicted to take 2nd place with only one model suggesting Ferrari could make a comeback.
-- Since the gap for 3rd is so small, the models are displaying disagreement over which team will earn this spot. Given Max Verstappen / Red Bull Racing's recent string of podiums and wins and Ferrari's decline in performance, Red Bull Racing is favored to triumph.
-- As seen in other ensemble predictions, there is a battle for 6th, but Racing Bulls seems to have an edge over Aston Martin and other teams given their overall performance this season.
-- Finally, since Kick Sauber's form has been declining and Haas F1 Team's form has been steadily rising, the models are suggesting there is a chance for the Haas F1 Team to steal 8th place from Kick Sauber.
+- Since Mercedes had a tepid performance at the US Grand Prix, the models show an indication that Ferrari or even Red Bull Racing may being able to steal 2nd from them.
+- The gap for 3rd is already small, so the models are displaying disagreement over which team will earn this spot. The general prediction maintains that Ferrari stays just ahead of Red Bull Racing.
+- As the season closes in the final 5 races, there is also movement predicted for the mid-field teams: Aston Martin and Racing Bulls fighting over 6th and Kick Sauber fending off the Haas F1 Team for 8th.
 ''')
 
 st.header("v1 Model Ensemble Predictions")
@@ -49,8 +49,7 @@ st.subheader("*Fast to React - sensitive to recent races*")
 st.dataframe(V1_PREDICTIONS, width="content", hide_index=True)
 st.markdown('''
 Key Points:
-- Mercedes is generally predicted to take 2nd place, but two of the less accurate models suggest Ferrari could make a comeback since the gap isn't too large.
-- Despite what the Tifosi-biased models say, a majority of the models think Red Bull Racing could even top Ferrari for 3rd.
-- Midfield battles are heating up. While a majority of the models are in agreement that Williams will hold onto their 5th place, some are suggesting a sudden drop in performance, given recent race results, leaving the door open for other teams.
+- Given Mercedes' declining performance, these models present a strong indication that they will lose out on 2nd to either Red Bull Racing or Ferrari. The likely suggestion is Red Bull could end up in 2nd, Mercedes in 3rd, and Ferrari in 4th.
+- Midfield battles continue. While a majority of the models are in agreement that Williams will hold onto their 5th place, some are suggesting a sudden drop in performance, given recent race results, leaving the door open for Racing Bulls.
 - The v1 models are not as accurate in predicting lower ranked teams, but the general consensus is that Kick Sauber, Haas F1 Team, and Alpine are on the bottom.
 ''')
